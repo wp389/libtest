df1325e145bd350350c2a233491027e82261475f
王鹏 <wp@wangpengdeMacBook-Pro.local>
2018-01-25 17:27:36 +0800

    recovery a compile

--- demo/compile.sh
+++ demo/compile.sh
@@ -1,10 +1,12 @@
 echo "start to compile"
 rm obj/* -f
-#g++ pdcserver.cpp backend_ceph.cpp pipe.cpp type.cc pdc_lock.cpp -g -o obj/server -lpthread -Wl,-rpath=/lib64/ -L/lib64/ -lrbd -lrados  #2>obj/server.log
+#g++ pdcserver.cpp backend_ceph.cpp pipe.cpp type.cc pdc_lock.cpp -g -std=c++11 -o obj/server -lpthread -Wl,-rpath=/lib64/ -L/lib64/ -lrbd -lrados  #2>obj/server.log
 
-g++ pdcserver.cpp backend_ceph.cpp pipe.cpp type.cc pdc_lock.cpp -g -o obj/server -lpthread  -L/lib64/ -lrbd -lrados  #2>obj/server.log
+g++ pdcserver.cpp backend_ceph.cpp pipe.cpp type.cc pdc_lock.cpp -g -std=c++11 -o obj/server -lpthread  -L/lib64/ -lrbd -lrados  #2>obj/server.log
 
-g++ -shared -fPIC libpdc.cc pdcclient.cpp backend_client.cpp pipe.cpp type.cc pdc_lock.cpp -g -o obj/libpdc.so.1.0.0 -lpthread
+g++ -shared -fPIC libpdc.cc pdcclient.cpp backend_client.cpp pipe.cpp type.cc pdc_lock.cpp -g -std=c++11 -o obj/libpdc.so.1.0.0 -lpthread
+gcc test/test.c -o obj/vmtest  -Wl,-rpath=/pdc/lib64/ -L/pdc/lib64/ -lrbd -lrados
+
 ls obj/
 
 exit 0
